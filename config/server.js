c0a467f17c330a0b294b6986afc454c56a856902
Marcello Silvério <smarcelloc@gmail.com>
2020-09-24 11:27:43 -0300

    Implementação do Express-Validator

--- config/server.js
+++ config/server.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+// Configuração
 app.set('view engine', 'ejs')
 app.set('views', './app/Views')
 
+// Middlewares 
 app.use(express.static('./public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// Autoload
 consign()
   .then('config/db.js')
   .include('routes')
--- routes/noticia.js
+++ routes/noticia.js
@@ -1,7 +1,9 @@
 const NoticiaModel = require('../app/Models/NoticiaModel')
+const { check, validationResult } = require('express-validator')
 
 module.exports = (app) => {
   const noticiaModel = new NoticiaModel(app.config.db())
+  let noticia = []
 
   app.get('/noticia', (requisicao, resposta) => {
 
@@ -12,13 +14,24 @@ module.exports = (app) => {
   })
 
   app.get('/noticia/add', (requisicao, resposta) => {
-    resposta.render('noticia/form')
+    resposta.render('noticia/form', { noticia: noticia })
   })
 
-  app.post('/noticia/add', (requisicao, resposta) => {
-    let noticia = requisicao.body;
-    noticiaModel.insert(noticia, () => {
-      resposta.redirect('/noticia')
-    })
+  app.post('/noticia/add', [
+    check('titulo', 'é obrigátorio').notEmpty(),
+    check('conteudo', 'é obrigátorio').notEmpty(),
+    check('conteudo', 'deve conter entre 10 a 100 caracteres').isLength({ min: 5, max: 300 })
+  ], (requisicao, resposta) => {
+    noticia = requisicao.body;
+    const erroValidacao = validationResult(requisicao)
+
+    if (erroValidacao.isEmpty()) {
+      noticiaModel.insert(noticia, () => {
+        resposta.redirect('/noticia')
+      })
+    } else {
+      resposta.render('noticia/form', { validacao: erroValidacao.array(), noticia: noticia })
+    }
+
   })
 }
